7e4852267d5bf893dd0302bb2f73b160c8803712
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-05-24 22:18:14 -0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash 
 
 THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
@@ -29,18 +29,15 @@ printf "\n"
 log() {
   echo -e "\033[38;5;105m .               /|\,/ |\,  ,- _~,   /\|,/ \|,   - _,,     ,- _~,  -_ /\,\033[0;00m"
   echo -e "\033[38;5;142m||    _          /| || ||   (' /| /  /| || ||     -/  )   (' /| /   || ,,\033[0;00m"
- echo -e "\033[38;5;099m=||=  / \\        || || ||  ((  ||/=  || || ||    ~||_<   ((  || =  /|| /|.\033[0;00m"
+  echo -e "\033[38;5;099m=||=  / \\        || || ||  ((  ||/=  || || ||    ~||_<   ((  || =  /|| /|.\033[0;00m"
   echo -e "\033[38;5;034m||  || ||        ||=|= ||  ((  ||    ||=|= ||     || |\  (( |||    \||/-\,\033[0;00m"
   echo -e "\033[38;5;406m||  || ||       ~|| || ||   ( / |   ~|| || ||     ,/--||  (   |     ||  \.,\033[0;00m"
   echo -e "\033[38;5;129m||  \|_-|        |, |\,|\,   -____-  |, |\,|\,   _--_-'    -____- _---_-|.\033[0;00m"
          echo -e "\033[38;5;129m||   -_-_\033[0;00m"
             echo -e "\033[38;5;129m,_-_-\.\033[0;00m"
-  echo -e "\033[38;5;208m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\033[0;00m"
-  echo '>Channel : '"@tgMember"
-  echo '>Develop by '"@sajjad_021"
  }
-  
- install_luarocks() {
+
+install_luarocks() {
  echo -e "\e[38;5;142mInstalling LuaRocks\e"
   git clone https://github.com/keplerproject/luarocks.git
   cd luarocks
@@ -56,7 +53,7 @@ cd ..
 
 install_rocks() {
 echo -e "\e[38;5;105mInstall rocks service\e"
-rocks="luasocket luasec redis-lua lua-term serpent dkjson Lua-cURL multipart-post lanes xml fakeredis luaexpat luasec lbase64 luafilesystem lub redis-lua lua-cjson feedparser serpent"
+rocks="luasocket luasec redis-lua lua-term serpent dkjson Lua-cURL multipart-post lanes xml fakeredis luaexpat luasec lbase64 luafilesystem lub lua-cjson feedparser serpent"
     for rock in $rocks; do
       ./.luarocks/bin/luarocks install $rock
     done
@@ -68,7 +65,6 @@ echo -e "\e[38;5;099minstall telegram-cli\e"
     wget https://valtman.name/files/telegram-cli-1222
     mv telegram-cli-1222 telegram-cli
     chmod +x telegram-cli
-    chmod +x run.sh
 }
   
 install2() {
@@ -79,26 +75,24 @@ echo -e "\e[38;5;034mInstalling more dependencies\e"
     sudo apt-get install libstdc++6 -y
     sudo apt-get install lua-lgi -y
     sudo apt-get install libnotify-dev -y
-    install_luarocks
-    install_rocks
-    py
-    tg
-    echo -e "\e[38;5;046mInstalling packages successfully\033[0;00m"
-    log
-    help
 }
 
 install() {
 echo -e "\e[38;5;035mUpdating packages\e"
-  	sudo apt-get update -y
-	sudo apt-get upgrade -y
+   sudo apt-get update -y
+   sudo apt-get upgrade -y
 
 echo -e "\\e[38;5;129mInstalling dependencies\e"
 	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 	sudo apt-get install g++-4.7 -y c++-4.7 -y
 	sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
 install2
-clear
+install_luarocks
+install_rocks
+py
+tg
+log
+echo -e "\e[38;5;046mInstalling packages successfully\033[0;00m"
 }
 
 py() {
@@ -107,7 +101,7 @@ py() {
  sudo pip install redis
 }
 
-info() {
+inf() {
 memTotal_b=`free -b |grep Mem |awk '{print $2}'`
 memFree_b=`free -b |grep Mem |awk '{print $4}'`
 memBuffer_b=`free -b |grep Mem |awk '{print $6}'`
@@ -123,49 +117,85 @@ ProcessCnt=`ps -A | wc -l`
 memUsed_b=$(($memTotal_b-$memFree_b-$memBuffer_b-$memCache_b))
 memUsed_m=$(($memTotal_m-$memFree_m-$memBuffer_m-$memCache_m))
 memUsedPrc=$((($memUsed_b*100)/$memTotal_b))
-echo ">Server Information"
+echo -e "➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\033[38;5;208mServer Information\033[0;00m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖"
 echo ">Total Ram : $memTotal_m MB  Ram in use : $memUsed_m MB - $memUsedPrc% used!"
 echo '>Cpu in use : '"$CPUPer"'%'
 echo '>Server Uptime : '"$uptime"
-echo -e "\033[38;5;208m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\033[0;00m"
-echo '>Channel : '"@tgMember"
-echo '>Develop by '"@sajjad_021"
 }
 
+anticrash() {
+while true ; do
+    tmux kill-session -t $THIS_DIR
+    tmux new-session -s $THIS_DIR "./telegram-cli -s tgGuard.lua"
+    tmux detach -s $THIS_DIR
+done
+}
 
-
-help() {
-echo -e "for see help use => \033[38;5;208m./make.sh help\033[0;00m"
-echo -e "for install packages use => \033[38;5;208m./make.sh install\033[0;00m"
-echo -e "for see your server info use => \033[38;5;208m./make.sh info\033[0;00m"
-echo -e "\033[38;5;037mOr\033[0;00m"
-echo -e "you can use the => \033[38;5;208m./make.sh\033[0;00m <= command for run your robot."
-echo -e "\033[38;5;208m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\033[0;00m"
+menu() {
+echo -e "➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\033[38;5;208mMENU\033[0;00m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖"
+echo -e "1 => \033[38;5;208mInstall\033[0;00m"
+echo -e "2 => \033[38;5;208mlaunch robot\033[0;00m"
+echo -e "3 => \033[38;5;208manticrash\033[0;00m"
+echo -e "4 => \033[38;5;208mTrun Off Robot\033[0;00m"
+echo -e "5 => \033[38;5;208mReturn last session\033[0;00m"
+echo -e "6 => \033[38;5;208mServer info\033[0;00m"
+echo -e "7 => \033[38;5;208mUninstall bot\033[0;00m"
+echo -e "0 => \033[38;5;208mExit\033[0;00m"
+echo -e "\033[38;5;208m  ➖  ➖  ➖➖➖➖➖➖  ➖  ➖  ➖  \033[0;00m"
 echo '>Channel : '"@tgMember"
 echo '>Develop by '"@sajjad_021"
+# Options in ./config.sh <option>
+read VAR
+if [ "$VAR" = 1 ]; then
+  clear
+  logo
+  install
+  menu
+elif [ "$VAR" = 2 ]; then
+	tmux new-session -s $THIS_DIR "./telegram-cli -s tgGuard.lua"
+elif [ "$VAR" = 3 ]; then
+ 	log
+	anticrash
+	menu
+elif [ "$VAR" = 4 ]; then
+	killall telegram-cli
+	tmux kill-session -t $THIS_DIR
+	log
+	echo -e '\e[34mSessions closed\e[0m'
+	menu
+elif [ "$VAR" = 5 ]; then
+	tmux attach-session -t $THIS_DIR
+elif [ "$VAR" = 6 ]; then
+  log
+	inf
+	menu
+elif [ "$VAR" = 7 ]; then
+  cd $home && rm -rf .telegram-cli && rm -rf tgGuard
+  menu
+elif [ "$VAR" = 0 ]; then
+	clear
+	log
+	exit
+elif [ "$VAR" = "" ]; then
+	clear
+	echo -e '\e[31mOpcion invalida\e[0m'
+	else
+	clear
+	echo -e '\e[31mOpcion invalida\e[0m'
+fi
 }
+
+if [ ! "$1" ]; then
+  menu
+fi
 
 if [ "$1" = "info" ]; then
-info
+  clear
+	log
+	inf
 fi
 
-if [ "$1" = "help" ]; then
-help
+if [ "$1" = "tmux" ]; then
+	log
+  anticrash
 fi
-
-if [ "$1" = "install" ]; then
-install
-fi
-
-if [ ! -f ./telegram-cli ]; then
-    echo -e "\033[38;5;208mError! telegram-cli not found, Please reply to this message:\033[1;208m"
-    read -p "Do you want to install and config? [y/n] = "
-	if [ "$REPLY" == "y" ] || [ "$REPLY" == "Y" ]; then
-       logo
-        install
-    elif [ "$REPLY" == "n" ] || [ "$REPLY" == "N" ]; then
-        exit 1
-  fi
-fi
-
-screen ./run.sh
