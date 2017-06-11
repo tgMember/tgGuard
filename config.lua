a65a95b16056a77ab3837c468efedae5beaf12b9
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-06-11 10:15:01 +0430

    Delete config.lua

--- config.lua
+++
@@ -1,4 +0,0 @@
-BOTS = 361871436 --[[Enter robot id here]]
-bot_id = 361871436 --[[Enter robot id here]]
-bot_owner = 158955285 --[[Enter your id here]]
-sudo_users = {158955285,279700027,180191663,361871436} --[[Enter yourId and robotId here]]
