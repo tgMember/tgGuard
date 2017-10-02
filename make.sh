34913adca49e177a6b323a8528c35ab873d06b69
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-10-02 12:20:15 -0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -49,12 +49,9 @@ lualibs=(
 'lua-cjson'
 'fakeredis'
 'xml'
-'dkjson'
 'feedparser'
 'serpent'
 )
-
-basepkg="libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson* libpython-dev make autoconf unzip git redis-server g++ libnotify-dev libstdc++6 lua-lgi luarocks libexpat1-dev wget unzip c++ tmux openssl coreutils libc6 libpcre3-dev libreadline-dev lua-socket lua-sec lua-expat"
 
 pkg=(
 'libreadline-dev'
@@ -63,31 +60,24 @@ pkg=(
 'lua5.2'
 'liblua5.2-dev'
 'libevent-dev'
-'libjansson*'
-'libpython-dev'
+'lua-sec'
+'lua-lgi'
+'openssl'
+'gcc'
 'make'
-'autoconf'
 'unzip'
 'git'
-'redis-server'
-'g++'
-'libnotify-dev'
-'libstdc++6'
-'lua-lgi'
-'luarocks'
-'libexpat1-dev'
 'wget'
-'unzip'
+'redis-server'
 'c++'
-'tmux'
-'openssl'
-'coreutils'
-'libc6'
-'libpcre3-dev'
-'libreadline-dev'
-'lua-socket'
-'lua-sec'
-'lua-expat'
+'g++'
+'libjansson-dev'
+'libpython-dev'
+'libnotify-dev'
+'autoconf'
+'expat'
+'libexpat1-dev'
+'luarocks'
 )
 
 today=`date +%F`
@@ -136,57 +126,62 @@ function configure() {
     cd luarocks-${luarocks_version}
     if [[ ${1} == "--no-null" ]]; then
         ./configure --prefix=$dir/.luarocks --sysconfdir=$dir/.luarocks/luarocks --force-config
-        make bootstrap
+	      make bootstrap
+				make build
+				make install
     else
         ./configure --prefix=$dir/.luarocks --sysconfdir=$dir/.luarocks/luarocks --force-config &>/dev/null
-        make bootstrap &>/dev/null
+	      make bootstrap &>/dev/null
+				make build &>/dev/null
+				make install &>/dev/null
     fi
     cd ..; rm -rf luarocks*
     if [[ ${1} != "--no-download" ]]; then
         download_libs_lua
     fi
-    for ((i=0;i<101;i++)); do
+		for ((i=0;i<101;i++)); do
         printf "\rConfiguring... [%i%%]" $i
         sleep 0.007
     done
 }
 
 function installation() {
-for i in $(seq 1 100); do  
-    sleep 0.05
-    sudo apt-get install $basepkg  -y --force-yes &>/dev/null
+    local i
+    for ((i=0;i<${#pkg[@]};i++)); do
+        sudo apt-get install ${pkg[$i]} -y --force-yes &>> .install-log${today}.txt
+    sleep 0.25
     if [ $i -eq 100 ]; then
-        echo -e "XXX\n100\n\033[1;33mInstall Luarocks and Download Libs\033[0;00m\nXXX"
-    elif [ $(($i % 4)) -eq 0 ]; then
+        echo -e "XXX\n100\nInstall Luarocks and Download Libs\nXXX"
+    elif [ $(($i % 1)) -eq 0 ]; then
         let "is = $i / 4"
         echo -e "XXX\n$i\n${pkg[is]}\nXXX"
     else
         echo $i
     fi 
-done | whiptail --title 'TeleGram Guard robot Install and Configuration' --gauge "${pkg[0]}" 6 60 0
+done | whiptail --title 'TeleGram Advertising bot Install and Configuration' --gauge "${pkg[0]}" 6 60 0
 }
 
-api() {
-	 echo -e "\n\033[38;5;27mPut your Token\n\033[38;5;208m\n\033[6;48m\n"
+function api() {
+echo -e "\n\033[38;5;27mPut your Token\n\033[38;5;208m\n\033[6;48m\n"
 read -rp '' TKN
- echo "#!/bin/bash
-	while true; do
-       		tmux kill-session -t tgGuard
-			tmux new-session -s tgGuard './telegram-cli --disable-link-preview -R -C -s tgGuard.lua -p tgGuard --bot=$TKN -L log.txt'
-        	tmux detach -s tgGuard
-	done" >> start
+echo "#!/bin/bash
+while true; do
+	tmux kill-session -t tgGuard
+		tmux new-session -s tgGuard './telegram-cli --disable-link-preview -R -C -s tgGuard.lua -p tgGuard --bot=$TKN -L log.txt'
+	tmux detach -s tgGuard
+done" >> start
 }
 
-cli() {
-    echo "#!/bin/bash
-     while true; do
-       sudo tmux kill-session -t tgGuard
-		sudo tmux new-session -s tgGuard './telegram-cli -W -R -C -v -s tgGuard.lua -p tgGuard -L log.txt'
-        sudo tmux detach -s tgGuard
-	done" >> start
+function cli() {
+echo "#!/bin/bash
+while true; do
+     	tmux kill-session -t tgGuard
+		tmux new-session -s tgGuard './telegram-cli -W -R -C -v -s tgGuard.lua -p tgGuard -L log.txt'
+	tmux detach -s tgGuard
+done" >> start
 }
 
-conf() {
+function conf() {
 AP="$THIS_DIR"/start
 if [ ! -f $AP ]; then
  echo -e "\n\033[1;32mA\033[0;00m for config api \033[1;34m<=API & CLI=>\033[0;00m for config cli bot \033[1;32mC\033[0;00m\n"
@@ -212,17 +207,25 @@ COUNTER=0
 }
 
 if [ ! -f "telegram-cli" ]; then
-  	chmod 777 make.sh
-	logo
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y &>/dev/null
+sudo apt-get autoclean -y &>/dev/null
+logo
+sudo apt-get update -y &>/dev/null
+	sudo apt-get -y remove lua5.3 &>/dev/null
+	echo -e "\n\033[1;31mPlease Waite ... \033[0;00m\n"
+	sudo apt-get -y update &>/dev/null; sudo apt-get upgrade -y --force-yes &>/dev/null; sudo apt-get dist-upgrade -y --force-yes &>/dev/null
 	dpkg -a --configure
-	sudo apt-get -y update &>/dev/null; sudo apt-get upgrade -y --force-yes &>/dev/null; sudo apt-get dist-upgrade -y --force-yes &>/dev/null; sudo apt-get -y install f &>/dev/null
-	installation
+ 	chmod 777 TG
+	installation ${@}
 	rm -rf README.md
- 	configure
-	wget "https://valtman.name/files/telegram-cli-1222" &>/dev/null
-	rm -rd logs
+ 	configure ${2}
+	sudo apt-get -y update &>/dev/null; sudo apt-get upgrade -y --force-yes &>/dev/null; sudo apt-get dist-upgrade -y --force-yes &>/dev/null
+	wget "https://valtman.name/files/telegram-cli-1222" 2>&1 | get_sub &>/dev/null
 	mv telegram-cli-1222 telegram-cli; chmod +x telegram-cli
-	conf
+  echo -e "\n\033[1;32mInstall Complete\033[0;00m\n"
+ 	sudo service redis-server restart
+ 	sudo service redis-server start
+  echo -e "\nCreate and Launch bot => \033[1;37mscreen -S nohup ./TG 1~9\033[0;00m\n"
 else
 	start
 fi
